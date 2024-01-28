9f96cb47f18453f9a9f694ffb8635f71dbbe75ea
chiedozie <chiedozieezenwa@gmail.com>
2024-01-28 20:08:50 +0100

    method to add movies, rent movies, check for availability

--- index.js
+++ index.js
@@ -1,15 +1,71 @@
-class Videoclub {
-  contructor(name, genre, countryOfOrigin, starAct) {
-    this.name = name;
+class Movie {
+  contructor(title, genre, countryOfOrigin, starAct, yearOfProduction) {
+    this.title = title;
     this.genre = genre;
     this.countryOfOrigin = countryOfOrigin;
     this.starAct = starAct;
+    this.yearOfProduction = yearOfProduction;
     this.available = null;
   }
-  rentMovie() {
-    if (this.available) {
-      this.available = false;
-      console.log(`"${this.name}" has been rented`);
+}
+
+class videoClub {
+  constructor() {
+    this.movies = [];
+  }
+
+  addFilm(title, genre, countryOfOrigin, starAct, yearOfProduction) {
+    const film = new Film(
+      title,
+      genre,
+      countryOfOrigin,
+      starAct,
+      yearOfProduction
+    );
+    this.movies.push(film);
+    console.log(`"${title}" has been added to the store.`);
+  }
+
+  rentMovie(title) {
+    const movie = this.movies.find((movie) => movie.title === title);
+    if (movie) {
+      if (movie.available) {
+        movie.available = false;
+        console.log(`"${title}" has been rented.`);
+      } else {
+        console.log(`Sorry, "${title}" is not available for rent.`);
+      }
+    } else {
+      console.log(`"${title}" is not available in the store.`);
+    }
+  }
+
+  returnMovie(title) {
+    const movie = this.movies.find((movie) => movie.title === title);
+    if (movie) {
+      if (!movie.available) {
+        movie.available = true;
+        console.log(`"${title}" has been returned.`);
+      } else {
+        console.log(`"${title}" is already available.`);
+      }
+    } else {
+      console.log(`"${title}" is not available in the store.`);
+    }
+  }
+
+  checkAvailability(title) {
+    const movie = this.movies.find((movie) => movie.title === title);
+    if (movie) {
+      if (movie.available) {
+        console.log(`"${title}" is available for rent.`);
+      } else {
+        console.log(`"${title}" is not available for rent.`);
+      }
+    } else {
+      console.log(`"${title}" is not available in the store.`);
     }
   }
 }
+
+const videoClub = new videoClub();
